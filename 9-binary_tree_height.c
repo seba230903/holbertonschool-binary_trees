50da023667c3ad89574c5f8e81f56177480656c0
WololoRC <wololoRC@proton.me>
2023-01-13 16:07:48 -0300

    9fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,24 +1,31 @@
 #include "binary_trees.h"
 /**
  * binary_tree_height - checks the height of a node
- * @tree: tree node
+ *
+ *@tree: tree node
+ *
  * Return: 0 on failure, height(@cnt) on success.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        int cnt = 0;
-        if (tree)
-        {
-                if (tree->left)
-                {
-                        binary_tree_height(tree->left);
-                        cnt++;
-                }
-                if (tree->right)
-                {
-                        binary_tree_height(tree->right);
-                        cnt++;
-                }
-        }
-        return (cnt);
+	int r, l;
+
+	l = r = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+		{
+			l = binary_tree_height(tree->left) + 1;
+		}
+		
+		if (tree->right)
+		{
+			r = binary_tree_height(tree->right) + 1;
+		}
+	}
+	else
+		return (0);
+
+	return (l > r ? l : r);
 }
